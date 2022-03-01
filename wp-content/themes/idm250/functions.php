301d402a955a1d6fa83d8e4de2f1296d1aaaf903
megan-lam <72055777+megan-lam@users.noreply.github.com>
2022-03-01 01:08:48 -0500

    fix wp menu and style

--- wp-content/themes/idm250/functions.php
+++ wp-content/themes/idm250/functions.php
@@ -12,6 +12,7 @@ if (version_compare($GLOBALS['wp_version'], '5.4.2', '<')) {
 function include_styles()
 {
     // Example of including an external link
+    wp_enqueue_style('normalize', 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css');
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
     // Example of including a style local to your theme root
@@ -46,11 +47,49 @@ function register_theme_navigation()
 
 add_action('after_setup_theme', 'register_theme_navigation');
 
-//required files
-require get_template_directory() . '/includes/required.php';
 
-//Theme intialization
-require get_template_directory() . '/includes/setup.php';
+function idm_register_custom_post_type()
+{
+    $args = [
+        'label' => 'Project',
+        'labels' => [
+            'name' => 'Projects',
+            'singular_name' => 'Project'
+        ],
+        'supports' => [
+            'title',
+            'editor',
+            'author',
+            'thumbnail',
+            'excerpt',
+            'trackbacks',
+            'custom-fields',
+            'comments',
+            'revisions',
+            'page-attributes',
+            'post-formats'
+        ],
+        // 'taxonomies'            => ['category', 'post_tag'],
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'show_in_rest' => true,
+        'rewrite' => [
+            'slug' => 'projects'
+        ],
+        // Dash Icons https://developer.wordpress.org/resource/dashicons/#media-audio
+        'menu_icon' => 'dashicons-clipboard'
+        // 'menu_icon'             => get_stylesheet_directory_uri() . '/static/images/icons/industries.png'
+    ];
 
-//Custom post types
-require get_template_directory() . '/includes/post-types.php';
+    register_post_type('idm-projects', $args);
+}
+add_action('init', 'idm_register_custom_post_type');
--- wp-content/themes/idm250/single-idm-projects.php
+++ wp-content/themes/idm250/single-idm-projects.php
@@ -1,3 +1,14 @@
-<?php get_header();?></body>
-this is single-idm-projects file
+<?php get_header();?>
+
+  <div class="everything">
+
+    <div class="case_content">
+    <?php while (have_posts()) : the_post(); ?>
+
+        
+    <?php the_content(); ?>
+    <!-- end content -->
+
+<?php endwhile; ?>
+</div></div>
 <?php get_footer();?>
