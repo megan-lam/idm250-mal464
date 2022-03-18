f823e22ba0dfafbfbe3044009f874b63d46aec9b
megan-lam <72055777+megan-lam@users.noreply.github.com>
2022-03-18 00:09:31 -0400

    template and new stuff

---
+++ wp-content/themes/idm250/template-about.php
@@ -0,0 +1,21 @@
+<?php
+/* Template Name: About */
+get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+<div class="everything">
+
+<div class="about_content">
+  <h1 class=""><?php the_title(); ?>
+  </h1>
+
+  <?php the_post_thumbnail(); ?>
+
+  <div class="">
+    <!-- start content -->
+    <?php the_content(); ?>
+    <!-- end content -->
+  </div>
+</div>
+<?php endwhile; ?>
+<?php get_footer();?>
