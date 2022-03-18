4a443948e88d24215f554fda3938392ec3bd4f63
megan-lam <72055777+megan-lam@users.noreply.github.com>
2022-03-18 04:27:23 -0400

    oops

--- wp-content/themes/idm250/front-page.php
+++ wp-content/themes/idm250/front-page.php
@@ -1,9 +1,6 @@
 <?php get_header();?></body>
 <body class="home">
 <div class="intro_text">
-    <h1 class="intro">Hi, I'm Megan</h1>
-    <h2 class="intro">An aspiring UX/UI designer from NJ.</h2>
-
-    <a href="about_me.html">
-        <h3 class="intro">Get to know me </h3>
+    <?php the_content(); ?>
+</div>
 <?php get_footer();?>
